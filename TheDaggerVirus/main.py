11b36e0f1f826d085861063fc7503ff0b5e3b5db
Mr. David <dbishop4880@outlook.com>
2024-12-12 15:28:43 -0500

    Delete TheDaggerVirus directory

--- TheDaggerVirus/main.py
+++
@@ -1,2 +0,0 @@
-#version 0.2.1 (first version of The Dagger Virus)
-#wiped clean virus, I have permanently cleared this virus since I found out it's not very powerful
--- TheDaggerVirus/notes.py
+++
@@ -1,6 +0,0 @@
-#main.py: main scripting of the Virus
-#notes.py: taking notes of all testing and description of main.py scripts
-#run.java: extra runner scripts & applications for the Virus
-#testing.java: for all extra tests for java stuff
-
-#
