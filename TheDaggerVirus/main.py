f584522fcf106f69bfc691c3faef246b1c3722b5
Mr. David <dbishop4880@outlook.com>
2024-12-12 15:03:54 -0500

    Add files via upload

---
+++ TheDaggerVirus/main.py
@@ -0,0 +1,2 @@
+#version 0.1.1 (first version of The Dagger Virus)
+#wiped clean virus, I have permanently cleared this virus since I found out it's not very powerful
---
+++ TheDaggerVirus/notes.py
@@ -0,0 +1,6 @@
+#main.py: main scripting of the Virus
+#notes.py: taking notes of all testing and description of main.py scripts
+#run.java: extra runner scripts & applications for the Virus
+#testing.java: for all extra tests for java stuff
+
+#
