80abc09bb97a429048a216b623aebe93f8a12a8b
Mr. David <dbishop4880@outlook.com>
2024-12-12 15:06:03 -0500

    Update main.py

--- TheDaggerVirus/main.py
+++ TheDaggerVirus/main.py
@@ -1,2 +1,2 @@
-#version 0.1.1 (first version of The Dagger Virus)
+#version 0.2.1 (first version of The Dagger Virus)
 #wiped clean virus, I have permanently cleared this virus since I found out it's not very powerful
